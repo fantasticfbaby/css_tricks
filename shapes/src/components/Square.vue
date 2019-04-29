594261ec21312f8e2470beffc6eb3dc6e73a4c9d
fantasticfbaby <1092902073@qq.com>
2019-04-29 21:14:32 +0800

    4.29

---
+++ shapes/src/components/Square.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <div class="square"></div>
+        <code>
+            .square {
+            width: 100px;
+            height: 100px;
+            background-color: red;
+            }
+        </code>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Square"
+    }
+</script>
+
+<style scoped>
+    .square {
+        width: 100px;
+        height: 100px;
+        background-color: red;
+    }
+</style>
